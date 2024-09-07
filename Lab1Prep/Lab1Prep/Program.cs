5d7b2f254a10452dec4994b8bc0c88fda9f81354
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-09-07 19:30:09 +0200

    Lab1Prep: Update Program.cs
    Added PrintStringUntilSpecifiedCharacter(), PrintStringNewLineOnSpecifiedCharacter(), ReplaceEvenIndexAsSpecifiedCharacter(), PrintSpecifiedCharactersAsGreenAndRed(), PrintDoubleCharactersAsGreen()

--- Lab1Prep/Lab1Prep/Program.cs
+++ Lab1Prep/Lab1Prep/Program.cs
@@ -4,7 +4,96 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintDoubleCharactersAsGreen("Hello World!");
+        }
+        static void PrintStringUntilSpecifiedCharacter(string input, char stopChar = ' ')
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == stopChar)
+                {
+                    break;
+                }
+                Console.Write(input[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintStringNewLineOnSpecifiedCharacter(string input, char stopChar = ' ')
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == stopChar)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(input[i]);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static void ReplaceEvenIndexAsSpecifiedCharacter(string input, char replacementChar = '*')
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(replacementChar);
+                }
+                else
+                {
+                    Console.Write(input[i]);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintSpecifiedCharactersAsGreenAndRed(string input, char greenChar = 'o', char redChar = 'l')
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].ToString().ToUpper() == greenChar.ToString().ToUpper())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (input[i].ToString().ToUpper() == redChar.ToString().ToUpper())
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                Console.Write(input[i]);
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static void PrintDoubleCharactersAsGreen(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i < input.Length-1 && input[i] == input[i+1])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(input[i]);
+                    Console.Write(input[i+1]);
+                    i++;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write(input[i]);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
+
