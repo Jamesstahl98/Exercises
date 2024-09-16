98a4c90a7a494688a0f518d0854f3ed14edc9da7
Jamesstahl98 <92599615+Jamesstahl98@users.noreply.github.com>
2024-09-16 14:30:54 +0200

    New Project added: BasicOOP
    Added Class Person()
    Added Functions SetLength(), GetLength(), SetLength(), GetLength(), GetBMI(), GetFullName(), GetFullNameReverse(), GetSelfAndParents()

---
+++ BasicOOP/BasicOOP/Program.cs
@@ -0,0 +1,97 @@
+﻿var mySelf = new Person();
+mySelf.firstName = "James";
+mySelf.lastName = "Ståhl";
+mySelf.mother = new Person();
+mySelf.mother.firstName = "Åsa";
+mySelf.mother.lastName = "Ståhl";
+mySelf.father = new Person();
+
+mySelf.SetLength(1.81);
+mySelf.SetWeight(69);
+
+Console.WriteLine(mySelf.GetBMI());
+
+Console.WriteLine(mySelf.GetSelfAndParents());
+
+
+static Person[] CreateAndGetTwoPersons()
+{
+    Person[] persons = new Person[2];
+
+    var firstPerson = new Person();
+    var secondPerson = new Person();
+    persons[0] = firstPerson;
+    persons[1] = secondPerson;
+
+    return persons;
+}
+
+public class Person()
+{
+    public string firstName;
+    public string lastName;
+
+    public Person father;
+    public Person mother;
+
+    private double length;
+    private double weight;
+
+    public void SetLength(double length)
+    {
+        this.length = length;
+    }
+
+    public double GetLength()
+    {
+        return length;
+    }
+
+    public void SetWeight(double weight)
+    {
+        this.weight = weight;
+    }
+
+    public double GetWeight()
+    {
+        return weight;
+    }
+
+    public double GetBMI()
+    {
+        return weight / (Math.Pow(2, length));
+    }
+
+    public string GetFullName()
+    {
+        if (firstName == null)
+        {
+            return "unknown";
+        }
+
+        return firstName + " " + lastName;
+    }
+
+    public string GetFullName(string title)
+    {
+        if (firstName == null)
+        {
+            return "unknown";
+        }
+
+        return title + " " + firstName + " " + lastName;
+    }
+
+    public string GetFullNameReverse()
+    {
+        string fullName = GetFullName();
+        char[] fullNameChars = fullName.ToCharArray();
+        Array.Reverse(fullNameChars);
+        return new string(fullNameChars);
+    }
+
+    public string GetSelfAndParents()
+    {
+        return $"{GetFullName()} - Mother {mother.GetFullName()} - Father {father.GetFullName()}";
+    }
+}
