745b0eece59ba5cbb3bc9b0f41d36ca0d61e352f
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-09-23 15:27:18 +0200

    Update Program.cs
    Added Vehicle class and car subclass

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(new Car(Brand.Volvo, "V70", Color.Blue));
+
+enum Brand { Toyota, Volvo, Kia, Ferrari, Ford }
+enum Color { Red, Green, Blue, Yellow, Purple }
+
+class Vehicle
+{
+    public string Brand { get; set; }
+
+    public string Color { get; set; }
+
+    public Vehicle(Brand brand, Color color)
+    {
+        Brand = brand.ToString();
+        Color = color.ToString();
+    }
+
+    public Vehicle(Brand brand)
+    {
+        Brand = brand.ToString();
+        Color = "Red";
+    }
+
+    public Vehicle()
+    {
+        Brand = "Ford";
+        Color = "Red";
+    }
+
+    public override string ToString()
+    {
+        return $"A {Color} {Brand}";
+    }
+}
+
+class Car : Vehicle
+{
+    public string Model { get; set; }
+
+    public Car()
+    {
+        Brand = "Ford";
+        Model = "F150";
+        Color = "Red";
+    }
+
+    public Car(Brand brand, string model, Color color) : this(brand, color)
+    {
+        Model = model;
+    }
+
+    public Car(Brand brand, Color color) : this(color)
+    {
+        Brand = brand.ToString();
+    }
+
+    public Car(Color color)
+    {
+        Color = color.ToString();
+    }
+
+    public override string ToString()
+    {
+        return $"A {Color} {Model} from {Brand}";
+    }
+}
