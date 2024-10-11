ddeb7b5f42bf0d3f38d64d4c4ea7c1c438df61ef
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-10-11 11:12:36 +0200

    Update Program.cs
    Added peopleBetween20and40, peopleBetween20and40Above190Height, longerFirstNameThanLastName, peopleWithNameAndBMI, peopleWithBMI20to25, BMI20to25Original and userNamesList

--- LINQExcercises/LINQExcercises/Program.cs
+++ LINQExcercises/LINQExcercises/Program.cs
@@ -1,26 +1,71 @@
 ﻿var people = new[]
 {
-    new { FirstName = "Lars",   LastName = "Larsson",   Age = 25,   Height = 180,   Weight = 70 },
-    new { FirstName = "Sven",   LastName = "Svensson",  Age = 20,   Height = 175,   Weight = 65 },
-    new { FirstName = "Bengt",  LastName = "Bengtsson", Age = 30,   Height = 191,   Weight = 75 },
-    new { FirstName = "Sara",   LastName = "Andersson", Age = 32,   Height = 165,   Weight = 60 },
-    new { FirstName = "Ulla",   LastName = "Johansson", Age = 68,   Height = 185,   Weight = 62 },
-    new { FirstName = "Per",    LastName = "Persson",   Age = 83,   Height = 170,   Weight = 62 },
-    new { FirstName = "Olof",   LastName = "Olofsson",  Age = 62,   Height = 187,   Weight = 80 },
-    new { FirstName = "Anna",   LastName = "Jansson",   Age = 35,   Height = 170,   Weight = 67 },
+    new { FirstName = "Lars",       LastName = "Larsson",   Age = 25,   Height = 180,   Weight = 70 },
+    new { FirstName = "Sven",       LastName = "Svensson",  Age = 17,   Height = 175,   Weight = 65 },
+    new { FirstName = "Bengt",      LastName = "Bengtsson", Age = 30,   Height = 191,   Weight = 90 },
+    new { FirstName = "Sara",       LastName = "Andersson", Age = 32,   Height = 165,   Weight = 60 },
+    new { FirstName = "Ulla",       LastName = "Johansson", Age = 68,   Height = 185,   Weight = 62 },
+    new { FirstName = "Pernilla",   LastName = "Persson",   Age = 83,   Height = 170,   Weight = 62 },
+    new { FirstName = "Olof",       LastName = "Olofsson",  Age = 62,   Height = 187,   Weight = 100 },
+    new { FirstName = "Anna",       LastName = "Jansson",   Age = 35,   Height = 170,   Weight = 67 },
 };
 
-var peopleBetween20and40 = new List<object>();
-var peopleBetween20and40AgeAbove190Height = new List<object>();
+//***Excercise two***
+var peopleBetween20and40 = people.Where(p => p.Age >= 20 && p.Age <= 40).ToList();
+Console.WriteLine(peopleBetween20and40.Count);
 
-for (int i = 0; i < people.Length; i++)
+//***Excercise three***
+var peopleBetween20and40AgeAbove190Height = people.Where(p => p.Age >= 20 
+&& p.Age <= 40 
+&& p.Height > 190).ToList();
+Console.WriteLine(peopleBetween20and40AgeAbove190Height.Count);
+
+//***Excercise four***
+var longerFirstNameThanLastName = people
+    .Where(p => p.FirstName.Length >= p.LastName.Length)
+    .Select(p => new {p.FirstName, p.LastName} ).ToList();
+
+for (int i = 0; i < longerFirstNameThanLastName.Count; i++)
 {
-    if (people[i].Age >= 20 && people[i].Age <= 40)
-    {
-        peopleBetween20and40.Add(people[i]);
-        if (people[i].Height > 190)
-        {
-            peopleBetween20and40AgeAbove190Height.Add(people[i]);
-        }
-    }
+    Console.WriteLine($"{longerFirstNameThanLastName[i].FirstName} {longerFirstNameThanLastName[i].LastName}");
 }
+
+//***Excercise five***
+//BMI = kg/m2
+var peopleWithNameAndBMI = people
+    .Select(p => new { p.FirstName, BMI = (MathF.Round(p.Weight / ((p.Height * p.Height) / 10_000f), 2)) }).ToList();
+
+for (int i = 0; i < peopleWithNameAndBMI.Count; i++)
+{
+    Console.WriteLine($"{peopleWithNameAndBMI[i].FirstName} {peopleWithNameAndBMI[i].BMI}");
+}
+
+//***Excercise six***
+Console.WriteLine();
+var peopleWithBMI20to25 = peopleWithNameAndBMI
+    .Where(p => p.BMI >= 20 && p.BMI <= 25).ToList();
+for (int i = 0; i < peopleWithBMI20to25.Count; i++)
+{
+    Console.WriteLine($"{peopleWithBMI20to25[i].FirstName} {peopleWithBMI20to25[i].BMI}");
+}
+
+//***Excercise seven***
+Console.WriteLine();
+Func<int, int, float> BMI = (height, weight) => (MathF.Round(weight / ((height * height) / 10_000f), 2));
+var BMI20to25Original = people
+    .Where(p => BMI(p.Height, p.Weight) >= 20 && BMI(p.Height, p.Weight) <= 25).ToList();
+
+for (int i = 0; i < BMI20to25Original.Count; i++)
+{
+    Console.WriteLine(BMI20to25Original[i].FirstName);
+}
+
+//***Excercise eight***
+var userNamesList = people
+    .Select(p => new { UserName = p.FirstName + p.Age, Category = p.Age >= 18 ? "adult" : "child" }).ToList();
+
+for (int i = 0; i < userNamesList.Count; i++)
+{
+    Console.WriteLine($"{userNamesList[i].UserName} {userNamesList[i].Category}");
+}
+Console.ReadLine();
