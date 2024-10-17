23d0e35fcc43f132bd666cf15bb2a0902fe6c7b9
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-10-17 15:34:50 +0200

    Added new Project WPFDialogExercises

---
+++ WPFDialogExercises/WPFDialogExercises/Views/Exercise1and2.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDialogExercises.Views
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Exercise1and2 : Window
+    {
+        public Exercise1and2()
+        {
+            MessageBox.Show("Application is about to start!");
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Application started!");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?",
+                    "Close window",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question) == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ WPFDialogExercises/WPFDialogExercises/Views/Exercise3and4.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WPFDialogExercises.Views
+{
+    /// <summary>
+    /// Interaction logic for Exercise3and4.xaml
+    /// </summary>
+    public partial class Exercise3and4 : Window
+    {
+        public Exercise3and4()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Window(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            var result = dialog.ShowDialog();
+
+            if(result == true)
+            {
+                textBox.Text = File.ReadAllText(dialog.FileName);
+            }
+        }
+    }
+}
