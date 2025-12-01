18aa7961ad32b2f36f92f821b54bbaa21c71ad98
Jamesstahl98 <Jamesstahl556@gmail.com>
2025-12-01 11:09:40 +0100

    Add DemoApiGateway

---
+++ Ocelot/DemoApiGateway/DemoApiGateway/Controllers/Hello1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hello1.api.Controllers;
+
+[Route("api/[controller]")]
+public class Hello1Controller : Controller
+{
+    public IActionResult Get()
+    {
+        return Ok("Hello from Hello1 API!");
+    }
+}
---
+++ Ocelot/DemoApiGateway/Hello2.api/Controllers/Hello2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hello2.api.Controllers;
+
+[Route("api/[controller]")]
+public class Hello2Controller : Controller
+{
+    public IActionResult Get()
+    {
+        return Ok("Hello from Hello2 API!");
+    }
+}
